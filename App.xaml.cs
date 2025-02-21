9eea8c3f9e6edc1820a3dfbe90625b0195b54827
Logic <zqr10159@126.com>
2025-02-22 00:12:09 +0800

    Merge dev into master (forced)

---
+++ App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+using Serilog;
+
+namespace BluetoothClipboard;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+        
+        // 配置Serilog
+        Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Debug()
+            .WriteTo.File("logs/bluetooth_clipboard.log", 
+                rollingInterval: RollingInterval.Day,
+                retainedFileCountLimit: 7)
+            .CreateLogger();
+            
+        Log.Information("应用程序启动");
+    }
+    
+    protected override void OnExit(ExitEventArgs e)
+    {
+        Log.Information("应用程序关闭");
+        Log.CloseAndFlush();
+        base.OnExit(e);
+    }
+}
+
